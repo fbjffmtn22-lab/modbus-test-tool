7ffa253dfbdd825844b2d7b8115f914255975523
fbjffmtn22-lab <lwk960329@icloud.com>
2026-07-17 13:56:21 +0800

    Add preload.js

---
+++ preload.js
@@ -0,0 +1,31 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('modbusAPI', {
+    // 请求/响应式 API
+    connect: (params) => ipcRenderer.invoke('modbus:connect', params),
+    disconnect: () => ipcRenderer.invoke('modbus:disconnect'),
+    getStatus: () => ipcRenderer.invoke('modbus:status'),
+    read: (params) => ipcRenderer.invoke('modbus:read', params),
+    write: (params) => ipcRenderer.invoke('modbus:write', params),
+    getSerialPorts: () => ipcRenderer.invoke('modbus:serial-ports'),
+
+    // 轮询 - 主进程推送事件
+    startPoll: (params) => ipcRenderer.send('poll:start', params),
+    stopPoll: () => ipcRenderer.send('poll:stop'),
+
+    // 事件监听
+    onPollData: (callback) => {
+        ipcRenderer.on('poll:data', (_event, data) => callback(data));
+    },
+    onPollError: (callback) => {
+        ipcRenderer.on('poll:error', (_event, data) => callback(data));
+    },
+    onLog: (callback) => {
+        ipcRenderer.on('log', (_event, msg) => callback(msg));
+    },
+    removeAllListeners: () => {
+        ipcRenderer.removeAllListeners('poll:data');
+        ipcRenderer.removeAllListeners('poll:error');
+        ipcRenderer.removeAllListeners('log');
+    }
+});
